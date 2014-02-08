301545e95a43e38c51e9b69ea62b0db3b95b9f71
MattHamersky <hamerskymatthew@yahoo.com>
2014-02-08 11:17:38 -0800

    Updated Server/Client classes

--- src/Network/Client.java
+++ src/Network/Client.java
@@ -1,5 +1,83 @@
 package Network;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Client {
+	
+    ClientThread clientThread;
+
+    public void establishConnection() {
+    	
+    }
+
+    public void closeConnection() {
+        clientThread.shutDown();
+    }
+
+    public void writeData(String data) {
+
+    }
+
+    public Client(int port, String ip, CommsHandlerInterface commsHandlerInterface) {
+        super(port, ip, commsHandlerInterface);
+        clientThread = new ClientThread();
+    }
+
+    public class ClientThread extends Thread {
+        boolean keepRunning = true;
+        Queue<String> outgoingCommandQueue;
+
+
+        public ClientThread() {
+        	outgoingCommandQueue = new LinkedList<String>();
+        }
+        
+        synchronized public void shutDown() {
+            keepRunning = false;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            try {
+                Socket socket = new Socket(ip, 12345);
+                socket.setReuseAddress(true);
+                socket.setKeepAlive(true);
+
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(
+                                socket.getOutputStream()));
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(
+                                socket.getInputStream()));
+                while (keepRunning) {
+
+                    if (outgoingCommandQueue.size() > 0) {
+                        final String data = outgoingCommandQueue.remove();
+                        writer.write(data);
+                        writer.flush();
+                    }
+
+                    try {
+                        final String s = reader.readLine();
+
+                    } catch (Exception e) {
+                    }
+
+                }
+                writer.close();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
--- src/Network/Server.java
+++ src/Network/Server.java
@@ -1,5 +1,84 @@
 package Network;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import android.util.Log;
+
 public class Server {
+	
+    ServerThread serverThread;
+    
+    public Server(int port, CommsHandlerInterface commsHandlerInterface) {
+        super(port, commsHandlerInterface);
+        serverThread = new ServerThread();
+    }
+
+    public void establishConnection() {
+    }
+
+    public void writeData(String data) {
+    }
+
+    @Override
+    public void closeConnection() {
+        serverThread.shutDown();
+    }
+
+    public class ServerThread extends Thread {
+        boolean keepRunning = true;
+
+        synchronized public void shutDown() {
+            keepRunning = false;
+        }
+
+
+        @Override
+        public void run() {
+            super.run();
+            try {
+                ServerSocket serverSocket = new ServerSocket(12345);
+                serverSocket.setReuseAddress(true);
+                Log.i("workshop", "waiting for connection");
+
+                Socket clientSocket = serverSocket.accept();
+                clientSocket.setKeepAlive(true);
+                clientSocket.setTcpNoDelay(true);
+
+                clientSocket.setSoTimeout(10);
+
+                Log.i("workshop", "connection established");
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(
+                                clientSocket.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(
+                                clientSocket.getOutputStream()));
+
+
+                while (keepRunning) {
+
+                    try {
+                        String s = reader.readLine();
+                        Log.i("workshop", "read in " + String.valueOf(s));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+//
+
+                }
+
+                clientSocket.close();
+                serverSocket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
