e37719ed3a98a7a3976d9efbf6ddf1a3d4ff04fd
Varatep Buranintu <vb.code500@gmail.com>
2014-02-08 18:22:27 -0800

    implementing tts for splash screen

--- src/com/example/deadreckoning/SplashActivity.java
+++ src/com/example/deadreckoning/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.example.deadreckoning;
 
+import Handlers.TTSHandler;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,11 +16,17 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class SplashActivity extends Activity {
-	
+	private TTSHandler tts;
+	//private TextView splashText;
 	protected void onCreate(Bundle savedInstanceState) { 
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash_screen);
+		//splashText = (TextView)findViewById(R.id.splashText);
+		tts = new TTSHandler(this);
 		
+		tts.speakPhrase("test");
+		//tts.speakPhrase(splashText.toString());
 		Thread splashTimer = new Thread() {
 			public void run() {
 				try{
